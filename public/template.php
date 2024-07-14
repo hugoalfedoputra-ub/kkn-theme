129d50ad92ec4bb8b871693a2bc2c40f48701185
hugoalfedo <hugoalfedoputra@student.ub.ac.id>
2024-07-15 01:34:25 +0700

    Boilerplate

---
+++ public/template.php
@@ -0,0 +1,22 @@
+<!-- Tambahkan file ini pada folder dist setelah build -->
+
+<?php
+defined('BASEPATH') || exit('No direct script access allowed');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+    <script type="module" crossorigin src="/public/desa/themes/kkn/assets/index-2Nhj0A2H.js"></script>
+    <link rel="stylesheet" crossorigin href="/public/desa/themes/kkn/assets/index-MBJLkBE_.css">
+</head>
+
+<body>
+    <div id="app"></div>
+</body>
+
+</html>
