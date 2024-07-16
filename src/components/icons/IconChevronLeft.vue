c7c7d6f23ca8d824eb068ed45b055ff5f079ef2d
Fakhry224 <fakhryzahranhakim@gmail.com>
2024-07-16 11:55:52 +0700

    Adding Icons and Images

---
+++ src/components/icons/IconChevronLeft.vue
@@ -0,0 +1,16 @@
+<template>
+  <svg
+    class="feather feather-chevron-left"
+    fill="none"
+    height="24"
+    stroke="currentColor"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    stroke-width="2"
+    viewBox="0 0 24 24"
+    width="24"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <polyline points="15 18 9 12 15 6" />
+  </svg>
+</template>
