b4fdef75e79f2bd23529715f93ceec4633797230
Fakhry224 <fakhryzahranhakim@gmail.com>
2024-07-17 12:38:09 +0700

    Adding Interactive Map, Footer Component, and update Navbar

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,3 +1,14 @@
 <template>
-  <p>Copyright</p>
+  <div class="px-20 py-7 w-full bg-emerald-900 max-md:px-5 max-md:max-w-full">
+    <div class="flex gap-5 justify-between max-md:flex-col max-md:gap-0">
+      <div class="flex flex-col w-auto max-md:ml-0 max-md:w-full">
+        <div class="text-md font-semibold text-white max-md:mt-10 max-md:max-w-full">
+          ©2024 Pemerintah desa Butun
+        </div>
+      </div>
+      <div class="flex flex-col ml-5 w-auto max-md:ml-0 max-md:w-full">
+        <div class="text-md font-semibold text-white max-md:mt-10">E-mail: butun@kominfo.go.id</div>
+      </div>
+    </div>
+  </div>
 </template>
--- src/components/InteractiveMap.vue
+++ src/components/InteractiveMap.vue
@@ -16,7 +16,7 @@ export default {
 </script>
 
 <template>
-  <div class="mymap w-[37.5rem] h-[34.385rem]">
+  <div class="mymap">
     <l-map ref="map" v-model:zoom="zoom" :center="[47.41322, -1.219482]">
       <l-tile-layer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
