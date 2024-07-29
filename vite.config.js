2af949649cad15ac2aaac6e537cd33d2854e10f6
Moch. Gustav Ali Rafsandjani <105447119+qznr@users.noreply.github.com>
2024-07-29 10:59:16 +0700

    vite

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,20 @@
+/* eslint-disable no-undef */
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+const ASSET_URL = process.env.ASSET_URL || '';
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  base: `${ASSET_URL}/v2/`,
+  plugins: [
+    vue(),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  },
-  base: '/desa/themes/kkn/'
+  }
 })
