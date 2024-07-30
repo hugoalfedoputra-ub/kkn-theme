1e38a3938b743d88148a7fbf35a2989a1c3a2812
hugoalfedo <hugoalfedoputra@student.ub.ac.id>
2024-07-30 20:50:01 +0700

    Updated mishap in statistik-desa replacement

--- src/router/index.js
+++ src/router/index.js
@@ -87,7 +87,7 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/statistik-desaView.vue')
+      component: () => import('../views/StatistikView.vue')
     },
     {
       path: '/statistik-desa/kependudukan',
