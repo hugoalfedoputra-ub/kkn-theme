a16089297a59baa4c4f0c19c07a7556c17955aa2
Simon Fernandez Martua Raja Pandapotan Sitompul <128397587+MedievalGentleman@users.noreply.github.com>
2024-07-30 18:59:39 +0700

    fix map

--- src/components/InteractiveMap.vue
+++ src/components/InteractiveMap.vue
@@ -1,11 +1,9 @@
 <script>
 import 'leaflet/dist/leaflet.css'
-import { LMap, LTileLayer } from '@vue-leaflet/vue-leaflet'
+// import { LMap, LTileLayer } from '@vue-leaflet/vue-leaflet'
 
 export default {
   components: {
-    LMap,
-    LTileLayer
   },
   data() {
     return {
@@ -17,12 +15,10 @@ export default {
 
 <template>
   <div class="mymap z-0">
-    <l-map ref="map" v-model:zoom="zoom" :center="[-8.045467372064147, 112.32297477291522]">
-      <l-tile-layer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        layer-type="base"
-        name="OpenStreetMap"
-      ></l-tile-layer>
-    </l-map>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d21043.248702520592!2d112.30955174814527!3d-8.050089945754884!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e78923071429d39%3A0x74f5ff55c98c022c!2sButun%2C%20Gandusari%2C%20Blitar%20Regency%2C%20East%20Java!5e0!3m2!1sen!2sid!4v1722340601410!5m2!1sen!2sid" 
+    class="w-full h-full"
+    allowfullscree
+    loading="lazy" 
+    referrerpolicy="no-referrer-when-downgrade"></iframe>
   </div>
 </template>
